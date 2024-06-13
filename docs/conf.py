e061a81eb808a6d0194efc7fb1177e2a134d6571
Automated <actions@users.noreply.github.com>
2024-06-13 19:54:15 +0000

    Demo of cookiecutter-python-package @ 921ef05

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,7 @@ from pathlib import Path
 from typing import Any
 
 sys.path.insert(0, str(Path(__file__).resolve().parents[1]))
-import cookiecutter_python_package_demo  # noqa: E402
+import cookiecutter_python_package_demo
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
